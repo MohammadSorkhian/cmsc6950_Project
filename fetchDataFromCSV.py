ba0ce03b4a0d1e81fae5565c73ace2ff08e96315
MohammadSorkhian <msorkhiankho@mun.ca>
2020-07-31 12:29:33 -0230

    draw plot

--- fetchDataFromCSV.py
+++ fetchDataFromCSV.py
@@ -1,17 +1,22 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from autorank import autorank, create_report, plot_stats, latex_table
-from pprint import pprint
 
 df = pd.read_csv('WHO-COVID-19-global-data.csv', parse_dates=['Date_reported'])
-days = 5
-grouped_country = df.groupby(' Country')
-dic_lastDays = {"Date_reported":list(df['Date_reported'].tail(days))}
-for x, item in grouped_country:
-    dic_lastDays[x] = list(item[' New_cases'].tail(days))
-pd.set_option('display.max_columns', 10)
-df_newCases_lastDays = pd.DataFrame(dic_lastDays)
-res = autorank(df_newCases_lastDays.iloc[:, 1:5], alpha=0.05, verbose=False)
-print(df_newCases_lastDays.iloc[:, :5])
-print(res)
+# country_names = list(df.groupby(' Country').groups.keys())
+numLastDays = 5
+numTopCountires = 10
+pd.set_option('display.max_columns', 8)
+df_GroupedCountry = df.groupby(' Country')
+dic_LastDays = {"Date_reported":list(df['Date_reported'].tail(numLastDays))}
+for x, item in df_GroupedCountry:
+    dic_LastDays[x] = list(item[' New_cases'].tail(numLastDays))
+df_NewCasesLastDays = pd.DataFrame(dic_LastDays)
+df_TopCountries = df_NewCasesLastDays.iloc[:, 1:].sum().sort_values(ascending=False).head(numTopCountires)
+list_TopCountriesName = list(df_TopCountries.keys())
+df_NewCasesLastDaysTop = df_NewCasesLastDays[list_TopCountriesName]
+se_NewCasesLastDaysTopMean = df_NewCasesLastDaysTop.mean()
+se_NewCasesLastDaysTopMean.plot(kind='bar', y='Number of New Cases per Day', title=f'Top {numTopCountires} countries in last {numLastDays} days')
+plt.savefig('TopCountries.png')
+plt.show()
+res = autorank(df_NewCasesLastDaysTop, alpha=0.05, verbose=False)
